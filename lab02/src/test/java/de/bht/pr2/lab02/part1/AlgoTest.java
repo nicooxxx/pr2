17045a1a15e2296f91cdab8b6a47a27001b3bfd5
nicooxxx <nicox535@gmail.com>
2024-02-15 13:47:18 +0100

    Lab02 Tests am schreiben

--- lab02/src/test/java/de/bht/pr2/lab02/part1/AlgoTest.java
+++ lab02/src/test/java/de/bht/pr2/lab02/part1/AlgoTest.java
@@ -2,7 +2,11 @@ package de.bht.pr2.lab02.part1;
 import de.bht.pr2.lab02.part2.*;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.lang.reflect.Array;
+
 public class AlgoTest {
     @BeforeEach
     void setUp() {
+        Array array[] = new Array[]
+    }
 }
