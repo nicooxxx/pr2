77467f13648fdfa8564405f2068a427c84159ebe
nicooxxx <nicox535@gmail.com>
2024-02-15 13:45:01 +0100

    Lab02 Tests am schreiben

---
+++ lab02/src/test/java/de/bht/pr2/lab02/part1/AlgoTest.java
@@ -0,0 +1,8 @@
+package de.bht.pr2.lab02.part1;
+import de.bht.pr2.lab02.part2.*;
+import org.junit.jupiter.api.BeforeEach;
+
+public class AlgoTest {
+    @BeforeEach
+    void setUp() {
+}
--- lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
+++ lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
@@ -1,11 +1,36 @@
 package de.bht.pr2.lab02.part1;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import de.bht.pr2.lab02.part1.Student;
 class MyTest {
 
   @Test
-  void testSomething() {
+  void testStudentKonstruktor() throws NotPaidTuitionFeeException, RegistrationNumberException, WrongCourseOfStudiesException, StudentParseException {
+    Student student = new Student("Anna Alt,70001,Medieninformatik,312");
+    assertEquals("Student [name='Anna Alt, registrationNumber=70001, courseOfStudies='Medieninformatik]", student.toString());
+  }
+  @Test
+  void testStudentParseException() throws NotPaidTuitionFeeException, RegistrationNumberException, WrongCourseOfStudiesException, StudentParseException {
+
+    assertThrows(StudentParseException.class, ()-> new Student("Anna Alt,70001,Medieninformatik"));
+  }
+
+  @Test
+  void testStudentLänge() throws NotPaidTuitionFeeException, RegistrationNumberException, WrongCourseOfStudiesException, StudentParseException {
+    Student student = new Student("Anna Alt,70001,Medieninformatik");
+    assertEquals("Not enough parts in data: 'Anna Alt,70001,Medieninformatik'", student.toString());
 
   }
+  /*@Test
+  void testStandortKmStand() {
+    Car car = new Car("Audi", "gelb", "Berlin", 10000); // Arrange
+
+    car.drive("Hamburg", 300);                           // Act
+
+    assertEquals("Hamburg", car.getPosition());           // Assert
+    assertEquals(10300, car.getKmStatus());                // Assert
+  }*/
 }
